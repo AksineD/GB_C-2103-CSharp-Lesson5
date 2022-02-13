e59094602fe8aa6bc92c0cf55f35c7d8d9c45357
Denis Y <dyampolsky@yokohama-oht.com>
2022-02-13 20:56:22 +0200

    hw

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Program.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+var fileName = "TestTextFile.txt";
+
+var userText = GetUserInput();
+
+SaveToFile(userText, fileName);
+
+string GetUserInput()
+{
+    Console.WriteLine("Enter a custom string");
+    var result = Console.ReadLine();
+
+    return result;
+}
+
+void SaveToFile(string text, string filename)
+{
+    var fullPath = Path.Combine(Environment.CurrentDirectory, filename);
+
+    if (File.Exists(fullPath)) File.AppendAllText(fileName, text + Environment.NewLine);
+    else File.WriteAllText(fileName, text + Environment.NewLine);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Program.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+var fileName = "bin1.dat";
+
+Console.WriteLine("Enter an arbitrary set of numbers (0...255). Separate each number with space.");
+var data = Console.ReadLine();
+
+var tokens = data.Split(' ');
+
+var dataToSave = Array.ConvertAll(tokens, byte.Parse);
+
+using (var binWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+{
+    foreach (var item in dataToSave) binWriter.Write(item);
+}
